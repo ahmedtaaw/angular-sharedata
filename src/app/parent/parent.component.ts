e6c57941d91242075283fc181a2cf1786bf51fff
ahmedtaaw <ahmedtaaw@gmail.com>
2017-09-26 01:59:49 +0200

    viewchild
    view child allows one component to be injected to another

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ChildComponent } from "../child/child.component";
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements AfterViewInit {
 
   constructor() { }
+  @ViewChild(ChildComponent) child;
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    //we can access the message variable that wass defined in the child component
+    this.message=this.child.message;
   }
+  //view child allows one component to be injected to another
   message="hello World";
 
   receiveMessage($event){
